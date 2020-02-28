4e768e9560610b062aa0902fa571f2d3bb1abac9
Francisco <fgoinhas@companieshouse.gov.uk>
2020-02-28 13:43:37 +0000

    Fixed encoding

--- Session.ts
+++ Session.ts
@@ -1,20 +1,20 @@
 import { Keys } from './utils/Keys';
-import { Encoding } from './utils/Encoding';
+import { Encoding, generateRandomBytesBase64, generateSessionId, generateSignature } from './utils/Encoding';
 import { config } from './config/Config';
 
 export function createNewSession(sessionSecret: string, email: string, password: string): ISession {
 
     const accessToken: IAccessToken = {
-        [Keys.AccessToken]: Encoding.generateRandomBytesBase64(64),
+        [Keys.AccessToken]: generateRandomBytesBase64(64),
         [Keys.ExpiresIn]: 3600,
-        [Keys.RefreshToken]: Encoding.generateRandomBytesBase64(64),
+        [Keys.RefreshToken]: generateRandomBytesBase64(64),
         [Keys.TokenType]: 'Bearer',
     };
 
     const defaultUserProfile: IUserProfile = {
         [Keys.Email]: email,
         [Keys.Forename]: password,
-        [Keys.UserId]: Encoding.generateRandomBytesBase64(1),
+        [Keys.UserId]: generateRandomBytesBase64(1),
         [Keys.Locale]: 'GB_en',
         [Keys.Scope]: null,
         [Keys.Surname]: 'test',
@@ -26,13 +26,13 @@ export function createNewSession(sessionSecret: string, email: string, password:
         [Keys.SignedIn]: 1,
         [Keys.UserProfile]: defaultUserProfile
     };
-    const id = Encoding.generateSessionId()
+    const id = generateSessionId()
     return {
         [Keys.Id]: id,
-        [Keys.ClientSig]: Encoding.generateSignature(id, sessionSecret),
+        [Keys.ClientSig]: generateSignature(id, sessionSecret),
         [Keys.Hijacked]: null,
-        [Keys.OAuth2Nonce]: Encoding.generateRandomBytesBase64(64),
-        [Keys.ZXSKey]: Encoding.generateRandomBytesBase64(64),
+        [Keys.OAuth2Nonce]: generateRandomBytesBase64(64),
+        [Keys.ZXSKey]: generateRandomBytesBase64(64),
         [Keys.Expires]: Date.now() + config().SESSION_EXPIRATION_TIME * 1000,
         [Keys.LastAccess]: Date.now(),
         [Keys.Pst]: 'all',
--- utils/Encoding.ts
+++ utils/Encoding.ts
@@ -9,81 +9,55 @@ export enum EncondingConstant {
     _cookieValueLength = _signatureStart + _signatureLength
 
 }
-
 export class Encoding {
 
-
-    public static generateSignature(id: string, secret: string): string {
-        const adjustedId = id.substr(0, EncondingConstant._idOctets);
-        return this.generateSha1SumBase64(adjustedId + secret).substr(0, EncondingConstant._signatureLength);
-    }
-
-    public static generateSha1SumBase64(base: any): string {
-
-        return crypto
-            .createHash('sha1')
-            .update(base)
-            .digest('base64');
-    }
-
-    public static generateRandomBytesBase64(numBytes: number): string {
-        return crypto.randomBytes(numBytes).toString('base64');
-
-    }
-
-    public static async generateRandomBytesBase64Async(numBytes: number): Promise<string> {
-        const asyncFun = promisify(crypto.randomBytes);
-        return (await asyncFun(numBytes)).toString('base64');
-
-    }
-
-    private static decodeMsgpack = (data: any): any => {
-        const buffer = Buffer.from(data, 'base64')
-        return JSON.parse(msgpack5().decode(buffer));
+    public static encode = <T>(value: T): string => {
+        if (!value) {
+            throw new Error('Value to encode must be defined');
+        }
+        return Encoding.encodeMsgpack(value);
     };
 
     private static encodeMsgpack = (data: any): string => {
         return msgpack5().encode(JSON.stringify(data)).toString('base64');
     };
 
-    public static encode = <T>(object: T): string => {
-        return Encoding.encodeMsgpack(object);
-    };
-
     public static decode = (value: string): any => {
-
+        if (!value) {
+            throw new Error('Value to decode must be defined');
+        }
         return Encoding.decodeMsgpack(value);
     };
 
-    public static generateSessionId(): string {
-        return Encoding.generateRandomBytesBase64(EncondingConstant._idOctets);
-    }
-
-    public static unmarshall = <T>(object: T): any => {
-        const obj: any = {};
-        const thisObj: any = object;
-
-        const keys = Object.keys(thisObj).sort();
-
-        for (const i in keys) {
-            if (thisObj.hasOwnProperty(keys[i])) {
-                obj[keys[i]] = thisObj[keys[i]];
-            }
-        }
-
-        return obj;
+    private static decodeMsgpack = (data: any): any => {
+        const buffer = Buffer.from(data, 'base64');
+        return JSON.parse(msgpack5().decode(buffer));
     };
 
-    public static marshall = <T>(object: T, data: any): T => {
-        const populatedObject = object as any;
-        const keys = Object.keys(data).sort();
-
-        for (const i in keys) {
-            if (data.hasOwnProperty(keys[i])) {
-                populatedObject[keys[i]] = data[keys[i]];
-            }
-        }
-
-        return populatedObject;
-    };
 }
+
+export function generateSessionId(): string {
+    return generateRandomBytesBase64(EncondingConstant._idOctets);
+}
+
+export function generateRandomBytesBase64(numBytes: number): string {
+    return crypto.randomBytes(numBytes).toString('base64');
+}
+
+export function generateSignature(cookieString: string, secret: string): string {
+    const adjustedId = extractSessionId(cookieString);
+    const value = crypto
+        .createHash('sha1')
+        .update(adjustedId + secret)
+        .digest('base64');
+    return value.substr(0, value.indexOf('='));
+}
+
+export function extractSessionId(sessionCookie: string): string {
+    return sessionCookie.substring(0, EncondingConstant._signatureStart);
+}
+
+export function extractSignature(sessionCookie: string): string {
+    return sessionCookie.substring(EncondingConstant._signatureStart);
+}
+
